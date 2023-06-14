f7c645e41366929c3bc5157830055f220b60cd23
gattoramm <gattoramm@gmail.com>
2023-06-14 21:13:15 +0300

    Update SimplePriorities.java

--- src/p21_threads/examples/ex4_priority/SimplePriorities.java
+++ src/p21_threads/examples/ex4_priority/SimplePriorities.java
@@ -1,5 +1,8 @@
 package p21_threads.examples.ex4_priority;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class SimplePriorities implements Runnable{
     private int countDown = 5;
     private volatile double d;
@@ -16,6 +19,27 @@ public class SimplePriorities implements Runnable{
 
     @Override
     public void run() {
+        Thread.currentThread().setPriority(priority);
+        while (true) {
+            for (int i = 0; i < 10000; i++) {
+                d += (Math.PI + Math.E) / (double) i;
 
+                if (i % 1000 == 0)
+                    Thread.yield();
+            }
+            System.out.println(this);
+
+            if (--countDown == 0) return;
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 5; i++)
+            exec.execute(new SimplePriorities(Thread.MIN_PRIORITY));
+
+        exec.execute(new SimplePriorities(Thread.MAX_PRIORITY));
+        exec.shutdown();
     }
 }
